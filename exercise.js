b61c4d47a07e15fb8a335fbf728187c8858bbdb4
CamilleCaschili <64851527+CamilleCaschili@users.noreply.github.com>
2020-05-06 01:10:11 +0200

    Delete exercise.js

--- exercise.js
+++
@@ -1,7 +0,0 @@
-var dataRequest = new XMLHttpRequest();
-dataRequest.open('GET', 'dataset-example.json');
-dataRequest.onload = function() {
-    var allProducts = JSON.parse(dataRequest.responseText);
-    console.log(allProducts[0])
-};
-dataRequest.send();
